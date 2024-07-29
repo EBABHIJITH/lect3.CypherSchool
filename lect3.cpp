7f94752a69e443a1069da340eb6b47887078c676
EBABHIJITH <146056289+EBABHIJITH@users.noreply.github.com>
2024-07-29 15:25:06 +0530

    Add files via upload

---
+++ lect3.cpp
@@ -0,0 +1,14 @@
+What are variables and constants in C++:
+Variables are named storage locations that can hold values which may change during program execution, while constants hold values that do not change.
+
+How will the computer identify the kind of input we give:
+The computer identifies the kind of input based on the data type specified for the variable that stores the input.
+
+How will the computer store the input we give:
+The computer stores the input in memory locations allocated to the variables, using the specified data types to determine the amount and format of storage.
+
+
+
+
+
+
